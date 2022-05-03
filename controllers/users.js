c0e841425d424a6a5a69bf473406504b8741cb70
vlad-prog93 <vladka_@bk.ru>
2022-05-03 10:59:19 +0300

    fix after autotest3

--- controllers/users.js
+++ controllers/users.js
@@ -64,6 +64,9 @@ const handleErrors = (err, req, res, next) => {
   if (err.name === 'ValidationError') {
     return res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя.' });
   }
+  if (!err) {
+    return next();
+  }
   return res.status(500).send({ message: 'Ошибка по-умолчанию.' });
 };
 
