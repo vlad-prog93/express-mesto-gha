7f79eed4ed6c68d57c6aeda11e939876106ba43e
vlad-prog93 <vladka_@bk.ru>
2022-05-22 17:31:37 +0300

    test 6

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,8 +4,7 @@ const ApiErrors = require('../utils/apiErrors');
 const SECRET_KEY = 'HELLObro';
 const auth = async (req, res, next) => {
   try {
-    // const token = req.headers.authorization.split(' ')[1];
-    const token = req.headers.authorization;
+    const token = req.headers.cookie.split('=')[1];
     const decoded = jwt.verify(token, SECRET_KEY);
     if (decoded) {
       req.user = {
