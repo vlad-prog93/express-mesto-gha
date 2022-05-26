afae533a8088b38dc812344c9acbeae06f5e5682
vlad-prog93 <vladka_@bk.ru>
2022-05-26 19:55:39 +0300

    test 9

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,7 +1,7 @@
 const errorHandler = (err, req, res, next) => {
   const statusCode = err.code || 500;
 
-  const message = statusCode === 500 ? 'Ошибка по-умолчанию.': err.message
+  const message = statusCode === 500 ? 'Ошибка по-умолчанию.' : err.message;
   res.status(statusCode).send({ message });
 
   next();
